6e3f995cc48acb0a1951486827d06fcfaf1df67b
Tino <Tino.Schmiedel@web.de>
2026-06-16 08:12:14 +0200

    Version 0.6 Vorbereitung - Strömungsmodul angelegt

---
+++ current.py
@@ -0,0 +1,11 @@
+def stroemungs_score():
+
+    return 0, "Strömungsdaten noch nicht integriert"
+
+
+if __name__ == "__main__":
+
+    punkte, text = stroemungs_score()
+
+    print(text)
+    print(f"{punkte:+} Punkte")
